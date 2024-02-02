50b51cde0e3eb98e0e5b4c78f98c6c13377c06a2
Rajkiranb55 <141542918+Rajkiranb55@users.noreply.github.com>
2024-02-03 02:06:46 +0530

    update upload url

--- Skribble/server/controller/upload_image.js
+++ Skribble/server/controller/upload_image.js
@@ -4,7 +4,7 @@ const port = process.env.API_URL;
 const uploadImage = async (req, res) => {
   res.json({
     success: 1,
-    image_url: `${port}/${req.file.filename}`,
+    image_url: `http://localhost:8000/${req.file.filename}`,
   });
 };
 
